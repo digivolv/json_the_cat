de74a1426ee3af33d3543962f128f5c6dbab7686
Edwin Ly <edwinjly@gmail.com>
2022-01-20 15:44:15 -0500

    added modularity. breedfetcher.js now has a callback function exported to index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,20 @@
 const request = require("request");
 
-const userInput = process.argv.slice(2);
-const requestUrl =
-  "https://api.thecatapi.com/v1/breeds/search?q=" + userInput[0];
-// console.log(requestUrl);
-
-const fetchBreedDescription = () => {
+const fetchBreedDescription = (breedName, callback) => {
+  const requestUrl =
+    "https://api.thecatapi.com/v1/breeds/search?q=" + breedName;
   request(requestUrl, (error, response, body) => {
     if (error) {
-      console.log(error);
-      return error;
+      callback(error, null);
     }
     const data = JSON.parse(body);
     if (data.length === 0) {
-      console.log("Breed not found");
+      callback("Breed not found");
       return;
     }
     const description = data[0].description;
-    console.log(description);
-    console.log(typeof data);
+    callback(null, description);
   });
 };
+
+module.exports = { fetchBreedDescription };
